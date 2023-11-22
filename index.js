4ec4c1dd2f0e904f62371b51fb63b9070ab8a664
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2023-11-23 01:35:42 +0530

    chore: remove unused imports (on the whims on heroku, which threatens to delete account)

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const router = express.Router(); // Creates a new router object.
 const mongoose = require('mongoose'); // Node Tool for MongoDB
 const config = require('./config/database'); // Mongoose Config
 const authentication = require('./routes/authentication')(router); // Import Authentication Routes
-const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-const cors = require('cors'); // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-var path = require('path');
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property
 
 const PORT = process.env.PORT || 8080;
 console.log("Port is "+PORT);
